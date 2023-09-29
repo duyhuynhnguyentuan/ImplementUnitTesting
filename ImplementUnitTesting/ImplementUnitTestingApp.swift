94c4306be4c017f52e85621a8956e18fd63f6c1b
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-09-29 16:55:07 +0700

    Initial Commit

---
+++ ImplementUnitTesting/ImplementUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImplementUnitTestingApp.swift
+//  ImplementUnitTesting
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImplementUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
